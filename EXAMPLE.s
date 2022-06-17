1d7db85493d034fe87c285ad672dd7273004bbea
RITHIKA KATHIRVEL <89640200+RITHIKAKATHIR@users.noreply.github.com>
2022-06-17 19:05:30 +0530

    Add files via upload

---
+++ EXAMPLE.s
@@ -0,0 +1,9 @@
+.text
+main:
+ li $s0,1
+ move $a0,$s0
+ li $v0,1
+ syscall
+
+li $v0,10
+syscall 
---
+++ example_read.s
@@ -0,0 +1,17 @@
+.data
+msg: .asciiz "Enter a number"
+.text
+main:
+ la $a0,msg
+ li $v0,4
+ syscall
+
+ li $v0,5
+ syscall
+ 
+ move $a0,$v0
+ li $v0,1
+ syscall
+
+li $v0,10
+syscall
